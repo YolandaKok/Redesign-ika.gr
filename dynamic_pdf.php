8985fd5292b3013ca05813c0e1f2e07d19200a55
YolandaKok <yolanda.kokkinou@gmail.com>
2018-02-12 01:51:56 +0200

    example

--- dynamic_pdf.php
+++ dynamic_pdf.php
@@ -3,6 +3,9 @@
  require_once 'helpers/greek_to_uppercase.php';
 
  require_once 'login.php';
+ use pdflib\File;
+ header('Content-Type: text/plain');
+ require_once '../autoloader.php';
  $conn = new mysqli($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
  $file = new File('test.pdf');
  $file->getInformation()
