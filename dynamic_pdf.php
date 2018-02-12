559e1e1a2ec3a8e77a5182f28f37fa1b31a1dd5d
YolandaKok <yolanda.kokkinou@gmail.com>
2018-02-12 02:43:42 +0200

    install

--- dynamic_pdf.php
+++ dynamic_pdf.php
@@ -2,16 +2,30 @@
 
  require_once 'helpers/greek_to_uppercase.php';
 
- //require_once 'login.php';
+ require_once 'login.php';
  //header('Content-Type: text/plain');
- //$conn = new mysqli($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
+ $conn = new mysqli($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
+ if ($conn->connect_error) die ($conn->connect_error);
+ // Escape user inputs for security
+ // Take arguments from POST method
+ $app_id = $_GET["id"];
+ // Select from users where username and password
+ mysqli_query($conn, "SET NAMES 'utf8'");
 
+ $query = "SELECT * FROM users WHERE id=$app_id";
+ $res= $conn->query($query);
+ $res->data_seek(0);
+ $row = $res->fetch_assoc();
+ $name = $row['firstname'];
+ $name = grstrtoupper($name);
+ $surname = $row['lastname'];
+ $surname = grstrtoupper($surname);
  require('fpdf181/fpdf.php');
 
  $pdf = new FPDF();
  $pdf->AddPage();
  $pdf->SetFont('Arial','B',16);
- $pdf->Cell(40,10,'Hello World!');
+ $pdf->Cell(40,10,$surname);
  $pdf->Output();
 
 
