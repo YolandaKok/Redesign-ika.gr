79e20150e8115b99b8363dedf52a8c07ff9a4ae4
YolandaKok <yolanda.kokkinou@gmail.com>
2018-02-12 02:00:02 +0200

    ex

--- dynamic_pdf.php
+++ dynamic_pdf.php
@@ -3,13 +3,9 @@
  require_once 'helpers/greek_to_uppercase.php';
 
  //require_once 'login.php';
- use pdflib\File;
- header('Content-Type: text/plain');
- require_once '../autoloader.php';
+ //header('Content-Type: text/plain');
+ require_once '10usb/pdf-lib/autoloader.php';
  //$conn = new mysqli($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
  $file = new File('test.pdf');
- $file->getInformation()
- 	->setTitle('My PDF Library')
- 	->setSubject('How to create a pdf library')
- 	->setAuthor('10usb');
+
 ?>
